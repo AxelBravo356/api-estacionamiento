d478651654089e51aa581e3e3556b3db46afa1c2
Axl Bravo <axlbravo365@gmail.com>
2023-12-08 13:39:37 -0600

    Inicio de Pryecto

---
+++ src/main/java/com/ipn/mx/apiestacionamiento/ApiEstacionamientoApplication.java
@@ -0,0 +1,13 @@
+package com.ipn.mx.apiestacionamiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEstacionamientoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEstacionamientoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ipn/mx/apiestacionamiento/ApiEstacionamientoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipn.mx.apiestacionamiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEstacionamientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
